d67d25db1c0aa09db7de949e2354ab0824d2ec7a
Sheraff <me@florianpellet.com>
2026-06-22 12:34:57 +0200

    typo

--- server/api/githubRepositories.ts
+++ server/api/githubRepositories.ts
@@ -318,7 +318,7 @@ async function fetchContributedRepositoriesSliceFromApi(year: number) {
 }
 
 async function fetchContributedRepositoriesFromApi(): Promise<ContributedRepositoriesResponse> {
-	publicLog("[data] fetching gitub contributions")
+	publicLog("[data] fetching github contributions")
 	const years = await fetchContributionYearsFromApi()
 	const contributionSlices = await Promise.all(
 		years.map((year) => fetchContributedRepositoriesSliceFromApi(year)),
